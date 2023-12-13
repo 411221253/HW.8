ef840f6aed80f4d7cb96da7aa40739c1fc0f083e
411221253 <145320808+411221253@users.noreply.github.com>
2023-12-14 00:48:22 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,1 +1,19 @@
 #IF
+
+
+typedef struck axis{
+  int x;
+  int y;  
+} axis_t;
+
+typedef struck stack{
+  axis_t element[M_SIZE*M_SIZE];
+  int top;
+} stack_t;
+
+void push(stack_t *stack, axis_t axis);
+axis_t pop(stack_t *stack);
+int isEmpty(stack_t *stack);
+
+
+
