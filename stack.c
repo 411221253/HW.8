c3cac5f0cce4a47a927f86da8e88280a28fc55d1
411221253 <145320808+411221253@users.noreply.github.com>
2023-12-19 19:18:49 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,1 +1,32 @@
 #include "stack.h"
+
+
+void push(stack_t *stackPtr, axis_t element){
+  if(isFULL(stackPtr))
+    return;
+    
+  stackPtr->top++;
+  stackPtr->elements[stackPtr->top] = element;
+}
+
+axis_t pop(stack_t *stackPtr){
+  axis_t temp = {-1,-1};
+  if(isEmpty(stackPtr))
+    return temp;
+  return stackPtr->elements[stackPtr->top--];//等同下三行
+  //temp = stackPtr->elements[stackPtr->top];
+  //stackPtr->top--;
+  //return temp;
+}
+
+//判斷是否為空
+int isEmpty(stack_t *stackPtr){
+  if(stackPtr->top == -1)
+    return 1;
+  return 0;
+}
+
+//判斷是否滿了
+int isFULL(stack_t *stackPtr){
+  if(top)
+}
